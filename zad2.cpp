c72e8735ea3bd583b2803e58347df20557bf5d19
kiubis <kiubis96@gmail.com>
2017-06-20 15:27:12 +0200

    Update zad2.cpp

--- zad2.cpp
+++ zad2.cpp
@@ -1,25 +1,44 @@
 #include <iostream>
-
+#include <math.h>
+#define debug(x) cout << #x << ":" << x << endl;
 using namespace std;
 
-void wpisywanie (float **tab);
+void wpisywanie (double **tab);
+void wyswietl (double **tab);
 
 int main (){
 cout << "Autorem programu jest Piotr Cichon" << endl;
 
-float **tab = new float*[19];
-for(int i = 0 ; i < 4 ; i++)
-	tab[i]= new float[4];
+double **tab = new double*[19];
+for(int i = 0 ; i < 19 ; i++)
+	tab[i]= new double[5];
 
 wpisywanie(tab);
-
+wyswietl(tab);
+delete []tab;
 
 system("pause");
 return 0;
 }
 
-void wpisywanie (float **tab){
-for(int i = 0 ; i <=18 ; i++);
-(*tab)[i] = (float)i*10;
+void wpisywanie (double **tab){
+	
+for(int i = 0 ; i <19 ; i++){
+tab[i][0] = (double)i*10;
+}
+for(int i = 0 ; i < 19 ; i++){
+	tab[i][1] = sin(tab[i][0]*M_PI/180);
+	tab[i][2] = cos(tab[i][0]*M_PI/180);
+	tab[i][3] = tab[i][1]-tab[i][2];
+	tab[i][4] = tab[i][1]+tab[i][2];
+}
+}
+
+void wyswietl (double **tab){
+ for (int i = 0; i < 19 ; i++){
+ 	cout << tab[i][0] << endl;
+ 	cout << tab[i][1] << "\t" << tab[i][2]<< "\t" << tab[i][3]<< "\t" << tab[i][4];
+ 	cout << endl << endl;
+ }
 
 }
